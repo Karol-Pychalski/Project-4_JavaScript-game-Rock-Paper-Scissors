2c435e8027204d25a6118196f77dcf421a7daa67
karol Pychalski <k.p201x@gmail.com>
2021-02-10 01:02:33 +0100

    Update to script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Mój ruch to' + playerMove + 'Jeśli twój ruch to kamień, to przegrywasz!');
